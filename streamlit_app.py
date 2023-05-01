44cf44e3898835ce4aff118b3339aad2ac5da32c
pk052023 <132288224+pk052023@users.noreply.github.com>
2023-05-01 15:18:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,9 +25,11 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
-
+# write your own comment - using Pandas to normalize the JSON dataset 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - flatten the JSON data
+streamlit.dataframe(fruityvice_normalized)
 
 
 
