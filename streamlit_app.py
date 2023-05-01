ab8c30f4d07d9ead127139cb3708f88835b04197
pk052023 <132288224+pk052023@users.noreply.github.com>
2023-05-01 17:47:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,9 +45,6 @@ except URLError as e:
 
 streamlit.write('The user entered ', fruit_choice) 
 
-streamlit.stop()
-
-
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
          my_cur.execute("SELECT * from fruit_load_list")
