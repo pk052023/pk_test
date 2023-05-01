ce6e14fed174cef2e2adde6e98dbc728a96d354f
pk052023 <132288224+pk052023@users.noreply.github.com>
2023-05-01 17:44:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,12 +47,17 @@ streamlit.write('The user entered ', fruit_choice)
 
 streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("Fruit load list:")
-streamlit.dataframe(my_data_rows)
+
+def get_fruit_load_list():
+   with my_cur = my_cnx.cursor() as my_cur:
+      my_cur.execute("SELECT * from fruit_load_list")
+      return my_cur.fetchall()
+   
+# Add a button to fetch fruits 
+if streamlit.button('Get fruit load list'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows=get_fruit_load_list()
+   streamlit.dataframe(my_data_rows)
 
 # key in your favorutie fruit
 add_fruit_to_the_list = streamlit.text_input('What fruit would you like?','Kiwi') 
